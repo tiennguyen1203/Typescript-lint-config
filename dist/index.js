3d1d8d0ceca2fc19c3042b8e15703e79a0d4ab1d
TienNguyen <tien.nguyen@team.enouvo.com>
2021-04-01 21:41:05 +0700

    init

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+const world = "world";
+export function hello(word = world) {
+    return `Hello ${world}! `;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const world = 'world';
+
+export default function hello(word: string = world) {
+  return `Hello ${word}! `;
+}
+
+console.error('123123');
